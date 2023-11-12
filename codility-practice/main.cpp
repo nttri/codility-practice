ccaff3bace8a63889b19fc16b172843cb8fac0fa
tringuyen <bem0302@gmail.com>
2023-11-12 22:38:48 +0700

    Lesson 1: Iterations - BinaryGap

--- codility-practice/main.cpp
+++ codility-practice/main.cpp
@@ -6,9 +6,40 @@
 //
 
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+/* Lesson 1: Iterations - BinaryGap */
+
+string toBinary(int n)
+{
+    string r;
+    while(n!=0) {r=(n%2==0 ?"0":"1")+r; n/=2;}
+    return r;
+}
+
+int solution(int N) {
+    int max = 0, cnt = 0;
+    string strN = toBinary(N);
+    
+    if (N == 1) { return 0; }
+    
+    for (char ch : strN) {
+        if (ch == '1') {
+            if (cnt > max) {
+                max = cnt;
+            }
+            cnt = 0;
+        } else {
+            cnt++;
+        }
+    }
+    return max;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int res = solution(999);
+    cout << res;
     return 0;
 }
